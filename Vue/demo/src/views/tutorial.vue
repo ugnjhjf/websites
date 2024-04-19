23b62dd5f12a8a1873f342852ce0a50902cce6e1
Rokidna G Mac V <qq617462323@outlook.com>
2024-04-19 11:20:22 +0800

    20240419_1120 WWW | CSS+HTML tutorial, use of hover and emmet

--- Vue/demo/src/views/tutorial.vue
+++ Vue/demo/src/views/tutorial.vue
@@ -1,6 +1,35 @@
 <template>
-    <div class="about">
-      <h1>This is an tutorial page</h1>
+  <div class="about">
+    <h1>This is an tutorial page</h1>
+    <button oncline="handleClick()">Click me</button>
+    count = 0
+
+    <script>
+      handleClick(){
+              this.count= Number(this.count)+1
+
+            }
+    </script>
+    <div class="login">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
-  </template>
+    <div class="logInfo1"></div>
+    <div class="logInfo2"></div>
+    <div class="logInfo3"></div>
+    <div class="logInfo4"></div>
+    <div class="logInfo5"></div>
+
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+  </div>
+</template>
   
